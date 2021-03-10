cd81a790d127786185c17f2a987bd0f51bed896d
Jasper <jasper_hendrickx@hotmail.com>
2021-03-10 14:30:20 +0100

    Add header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Welcome to QualityQuilts</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to QualityQuilts</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
