a774d1c60998344e5fb4bbb36642684fb3e38ec5
Jasper <jasper_hendrickx@hotmail.com>
2021-03-10 15:25:59 +0100

    Create product card and render products

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Quilt 1',
+    image: '/images/quilt1.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id leo in vitae turpis massa sed elementum. Diam maecenas sed enim ut sem viverra aliquet.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 3,
+  },
+  {
+    _id: '2',
+    name: 'Quilt 2',
+    image: '/images/quilt2.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id leo in vitae turpis massa sed elementum. Diam maecenas sed enim ut sem viverra aliquet.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 3,
+  },
+  {
+    _id: '3',
+    name: 'Quilt 3',
+    image: '/images/quilt3.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id leo in vitae turpis massa sed elementum. Diam maecenas sed enim ut sem viverra aliquet.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 3,
+  },
+  {
+    _id: '4',
+    name: 'Quilt 4',
+    image: '/images/quilt4.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id leo in vitae turpis massa sed elementum. Diam maecenas sed enim ut sem viverra aliquet.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 3,
+  },
+  {
+    _id: '5',
+    name: 'Quilt 5',
+    image: '/images/quilt5.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id leo in vitae turpis massa sed elementum. Diam maecenas sed enim ut sem viverra aliquet.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 3,
+  },
+  {
+    _id: '6',
+    name: 'Quilt 6',
+    image: '/images/quilt6.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id leo in vitae turpis massa sed elementum. Diam maecenas sed enim ut sem viverra aliquet.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 3,
+  },
+];
+
+export default products;
