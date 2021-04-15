9bdb026d31609046eff3f8781458cf78fad284a5
Jasper <jasper_hendrickx@hotmail.com>
2021-04-15 23:33:21 +0200

    Add order screen

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -43,7 +43,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
 //@access   Private
 
 const getOrderById = asyncHandler(async (req, res) => {
-  const order = await (await Order.findById(req.params.id)).populate(
+  const order = await Order.findById(req.params.id).populate(
     'user',
     'name email'
   );
